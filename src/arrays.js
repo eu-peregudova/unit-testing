edb648679b9381694a55a72fa9097613d36ef820
Eugenia <peregoodovaeva@gmail.com>
2023-02-13 13:54:24 +0200

    feat: create class

---
+++ src/arrays.js
@@ -0,0 +1,34 @@
+class LodashArray {
+    chunk() {
+
+    }
+    compact() {
+
+    }
+    drop() {
+
+    }
+    dropWhile() {
+
+    }
+    take() {
+
+    }
+    filter() {
+
+    }
+    find() {
+
+    }
+    includes() {
+
+    }
+    map() {
+
+    }
+    zip() {
+
+    }
+}
+
+module.exports = LodashArray

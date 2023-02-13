6221e79c43873ec02300b884306c70bac52c6c5c
Eugenia <peregoodovaeva@gmail.com>
2023-02-13 14:20:37 +0200

    feat: add test for drop()

--- tests/arrays.test.js
+++ tests/arrays.test.js
@@ -47,3 +47,39 @@ describe('Lodash: compact', () => {
         expect(lodash.compact(testArray)).toBe(result)
     })
 })
+
+describe('Lodash: drop', () => {
+    let testArray;
+
+    beforeEach(() => {
+        testArray = [1, 2, 3, 4, 5]
+    })
+
+    test('should be defined', () => {
+        expect(lodash.drop).toBeDefined()
+        expect(lodash.drop).not.toBeUndefined()
+    })
+
+    test('should return an array', () => {
+        expect(lodash.drop()).toBeInstanceOf(Array)
+    })
+
+    test('default number of dropped elements is 1', () => {
+        expect(lodash.drop(testArray)).toBe([2, 3, 4, 5])
+        expect(lodash.drop(testArray)).toBe(lodash.drop(testArray, 1))
+    })
+
+    test('should return as is if number of dropped elements is 0', () => {
+        expect(lodash.drop(testArray, 0)).toBe([1, 2, 3, 4, 5])
+        expect(lodash.drop(testArray, 0)).toBe(testArray)
+    })
+
+    test('should creates a slice of array with n elements dropped from the beginning', () => {
+        expect(lodash.drop(testArray, 2)).toBe([3, 4, 5])
+        expect(lodash.drop(testArray, 3)).toBe([4, 5])
+        expect(lodash.drop(testArray, 4)).toBe([5])
+        expect(lodash.drop(testArray, 5)).toBe([])
+        expect(lodash.drop(testArray, 100)).toBe([])
+    })
+})
+

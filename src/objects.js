a3754436355cee15f218341e3d8fe1a0ca65621c
Eugenia <peregoodovaeva@gmail.com>
2023-02-26 18:55:13 +0200

    feat: omit() done

--- src/objects.js
+++ src/objects.js
@@ -1,10 +1,36 @@
+LodashArray = require('../src/arrays.js')
+let _ = new LodashArray
+
 class LodashObject {
     merge() {
 
     }
 
-    omit() {
+    /**
+     * The opposite of _.pick; this method creates an object composed of the own
+     * and inherited enumerable property paths of object that are not omitted.
+     *
+     *  @param {Object} object
+     *  @param {String, Array} paths
+     *
+     *  @return {Object}
+     */
 
+    omit(object, paths) {
+        if (typeof paths === 'string') {
+            const a = paths
+            paths = []
+            paths[0] = a
+        }
+        let result = {}
+        for (let key in object) {
+            if (!_.includes(paths, key)) {
+                console.log(paths)
+                console.log(key, _.includes(paths, key))
+                result[key] = object[key]
+            }
+        }
+        return result
     }
 
     omitBy() {
